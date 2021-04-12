d2731876b5d9a79f15bdf5cfca1fc59b09033eab
Vexedness <giorikas.bench@gmail.com>
2021-04-12 14:57:18 +0300

    Co-authored-by: georgesoul <georgesoul@users.noreply.github.com>

--- Dungeon_Master.py
+++ Dungeon_Master.py
@@ -2,7 +2,11 @@ import discord
 import requests
 import json
 import random
-# gamw thn panagia sou
+import pymongo
+
+dbclient = pymongo.MongoClient("mongodb+srv://Giorgos:cumzone@insultdb.xr2uz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = dbclient["insultdb"]
+
 client = discord.Client()
 
 happy_words = ["happy","glad","fantastic","good","amazing","cheerful"]
@@ -14,20 +18,6 @@ def get_quote():
     json_data = json.loads(response.text)
     quote = json_data['insult']
     return(quote)
-#SAVE INSULTS FROM API TO DB
-def update_insults(insulting_message):
-    if "insults" in db.key():
-        insults = db["insults"]
-        insults.append(insulting_message)
-        db["insults"] = insults
-    else:
-        db["insults"] = [insulting_message]
-#DELETE INSULTS FROM DB
-def delete_insults(index):
-    insults = db["insults"]
-    if len(insults) > index:
-        del insults[index]
-        db["insults"] = insults
 
 
 @client.event
@@ -40,38 +30,17 @@ async def on_message(message):
         return
 
     msg = message.content
-#SUP
+    
     if message.content.startswith('$sup'):
         await message.channel.send('Hey bro, nice cock!')
-#API COMMAND
+
     if message.content.startswith('$wisdom'):
         quote = get_quote()
         await message.channel.send(quote)
-#DB KINDA SORTA W/E
-    options = starter_insults
-    if "insults" in db.key():
-        options = options + db["insults"]
-#SAVE NEW INSULT
-    if msg.startswith("$new"):
-        insulting_message = msg.split("$new ",1)[1]
-        update_insults(insulting_message)
-        await message.channel.send("New insult has been added... Cunt.")
-#DELETE INSULT
-    if msg.startswith("del"):
-        insults = []
-        if "insults" in db.key():
-            index = int(msg.split("$del",1)[1])
-            delete_insults(index)
-            insults = db["insults"]
-        await message.channel.send(insults)
-#SEE INSULT LIST
-    if msg.startswith("list"):
-        insults = []
-        if "insults" in db.key():
-            insults = db["insults"]
-        await message.channel.send(insults)
-#KEY WORDS
-    if any(word in msg for word in happy_words):
-        await message.channel.send(random.choice(options))
+        db["gamostavridia"].update_one(
+                    { "insult" : quote },
+                    { "$set": { "insult": quote}},
+                    upsert = True  
+                 )
 
-client.run('ODI4NjAyNzgwOTUxMzgwMDI4.YGr-eQ.in_CP3R8-ZyUA7vNgbpgUScd-a8')
+client.run('ODI4NjAyNzgwOTUxMzgwMDI4.YGr-eQ.5aHzBNXTKy2ai9sprSg_mqTfEAo')
